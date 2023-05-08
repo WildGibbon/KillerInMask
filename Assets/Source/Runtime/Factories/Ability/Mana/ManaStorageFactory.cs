221f2e7f2e220370d90e2fd28fe4119b3b985ca2
Ainur Nugumanov <chelovekebola@mail.ru>
2023-05-09 01:27:11 +0500

    forgot to make an implementation of ManaStorageFactory:)

---
+++ Assets/Source/Runtime/Factories/Ability/Mana/ManaStorageFactory.cs
@@ -0,0 +1,23 @@
+﻿using MaskedKiller.Model.Ability;
+using MaskedKiller.Model.Ability.Mana;
+using UnityEngine;
+
+namespace MaskedKiller.Factories.Ability.Mana
+{
+	public class ManaStorageFactory : MonoBehaviour, IManaStorageFactory
+	{
+		[SerializeField] private int _maxValue;
+
+		private IManaStorage _instance;
+
+		public IManaStorage Create()
+		{
+			if(_instance != null)
+				return _instance;
+
+			_instance = new ManaStorage(_maxValue);
+
+			return _instance;
+		}
+	}
+}
